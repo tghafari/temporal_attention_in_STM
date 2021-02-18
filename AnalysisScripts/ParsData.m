f0eca4e97fdb5eea2f6b1c19e9042ca50b7ffbd0
tghafari <tara.ghafari@gmail.com>
2021-02-18 19:05:38 +0330

    parsdata updated

--- AnalysisScripts/ParsData.m
+++ AnalysisScripts/ParsData.m
@@ -2,25 +2,27 @@
 clc
 clear ;
 %% preallocation and file introduction
-SubjList =260:269;
+SubjList = setxor(24:43,[26,32]); %all 4diff subjects
+% 259:269; %quad data
 % [259]; %very good performance in all quadrants
-% 245:256; %experimental error
-setxor(24:43,[26,32,42,39]);
+% 245:258; %experimental error
+% setxor(24:43,[26,32,42,39]); %4difflev data (RAW poster data)
 AllData=cell(numel(SubjList),1);
 meanScore=zeros(1,numel(SubjList));
-% cd('/Users/Tara/Documents/MATLAB/MATLAB Programs/PhD Thesis Programs/DMS Project/Results/Analyse/DMS4DiffLevel/')
-% ColNames = {'code', 'foreperiod', 'predictability', 'difficulty', ...
-%     'response', 'reaction_time', 'correct_response', 'fp_check', 'ITI',...
-%     'ITI_check', 'delay_phase_check', 'repetition', 'stimulus','question', 'confidence', ...
-%     'confidence_RT', 'TCI','score','RTCorrect','ConfCorrect','TP','TN','FP','FN','xpos','ypos','perfGrp'};
-% VarsOfInterest = [2, 3, 4, 5, 6, 7, 13, 14, 15, 16]; %for 4difflev
 
-cd('/Users/Tara/Documents/MATLAB/MATLAB Programs/PhD Thesis Programs/DMS Project/Results/Analyse/DMSQuadrants/')
+cd('/Users/Tara/Documents/MATLAB/MATLAB-Programs/PhD-Thesis-Programs/DMS-Project/Results/Analyse/DMS4DiffLevel/')
 ColNames = {'code', 'foreperiod', 'predictability', 'difficulty', ...
     'response', 'reaction_time', 'correct_response', 'fp_check', 'ITI',...
     'ITI_check', 'delay_phase_check', 'repetition', 'stimulus','question', 'confidence', ...
-    'confidence_RT', 'TCI','Quad'};
-VarsOfInterest = [1,2, 3, 4, 5, 6, 7, 13, 14, 15, 16,18]; %for quads
+    'confidence_RT', 'TCI','score','RTCorrect','ConfCorrect','TP','TN','FP','FN','xpos','ypos','perfGrp'};
+VarsOfInterest = [1,2,3,4,5,6,7,13,14,15,16]; %for 4difflev
+
+% cd('/Users/Tara/Documents/MATLAB/MATLAB-Programs/PhD-Thesis-Programs/DMS-Project/Results/Analyse/DMSQuadrants/')
+% ColNames = {'code', 'foreperiod', 'predictability', 'difficulty', ...
+%     'response', 'reaction_time', 'correct_response', 'fp_check', 'ITI',...
+%     'ITI_check', 'delay_phase_check', 'repetition', 'stimulus','question', 'confidence', ...
+%     'confidence_RT', 'TCI','Quad','score','RTCorrect','ConfCorrect','TP','TN','FP','FN','xpos','ypos','perfGrp'};
+% VarsOfInterest = [1,2,3,4,5,6,7,13,14,15,16,18]; %for quads
  
 % cd('/Users/Tara/Documents/MATLAB/MATLAB Programs/PhD Thesis Programs/DMS Project/Results/Trainings/') %for trainings
 % ColNames = {'code', 'foreperiod', 'predictability', 'difficulty', ...
@@ -29,7 +31,7 @@ VarsOfInterest = [1,2, 3, 4, 5, 6, 7, 13, 14, 15, 16,18]; %for quads
 %     'confidence_RT', 'TCI','Quad','score','RTCorrect','ConfCorrect','TP','TN','FP','FN','xpos','ypos','perfGrp'};
 % VarsOfInterest = [2, 3, 4, 5, 6, 7, 13, 14, 15, 16]; %for training
 
-%% analysis part
+%% Preprocessing the data
 for sId = 1:numel(SubjList)
     fname = fullfile(sprintf('%d-1.mat',SubjList(sId))); %-2-1 for trainings
     load(fname);
@@ -41,6 +43,7 @@ for sId = 1:numel(SubjList)
     conditionsMat(conditionsMat(:,2) == tmp(3), :) = [];
     conditionsMat(conditionsMat(:,2) == max(conditionsMat(:,2)),2) = 1.4;
     
+    %Calculate performance
     Data = array2table(conditionsMat(:,VarsOfInterest),'VariableNames',ColNames(VarsOfInterest)); %for other than overtime you need to put conditionsMat to 20:end
     Data.score = Data.response == Data.correct_response;
     Data.RTCorrect=Data.reaction_time; Data.RTCorrect(Data.score==0)=nan;
@@ -50,33 +53,38 @@ for sId = 1:numel(SubjList)
     Data.FP = Data.response == 1 & Data.correct_response == 2;
     Data.FN = Data.response == 2 & Data.correct_response == 1;
     
-%     Data.diffForPos=Data.difficulty;
-%     if SubjList(sId)<250 %for the messed up 8 stimuli in quad task
-%         Data.diffForPos(Data.code>352)=4;
-%     end
+    %Calculate position
     Data.xpos=cos(2*pi*(Data.question-1)./Data.difficulty+pi./Data.difficulty);
     Data.ypos=sin(2*pi*(Data.question-1)./Data.difficulty+pi./Data.difficulty);
+    %Data.xpos=cos(2*pi*(Data.question-1)./Data.difficulty) %for 4diff
+    %Data.ypos=sin(2*pi*(Data.question-1)./Data.difficulty) %for 4diff
     Data.xpos(Data.correct_response==2)=nan;
     Data.ypos(Data.correct_response==2)=nan;
     
-    Data.perfGrp=nan(height(Data),1);
     %Label subjects regarding their performances
-        difficulty=unique(Data.difficulty);
-        meanScore(:,sId)=mean(Data.score);
-%         %(Data.difficulty==max(difficulty) & Data.foreperiod==.65 & (Data.predictability==1));
-%     for diffLev=1:length(difficulty)
-%         if ll>=0.5
-%             Data.perfGrp(Data.difficulty==difficulty(diffLev))=6; %perfect
-%         elseif ll>=0.7
-%             Data.perfGrp(Data.difficulty==difficulty(diffLev))=8; %good
-%         elseif ll>0.55
+    Data.perfGrp = nan(height(Data),1);
+    difficulty   = unique(Data.difficulty);    
+    %Decide in respect to which variable you want to group performance
+    meanScore(:,sId) = median(Data.score(Data.difficulty==4));
+%     & Data.foreperiod==.65 & Data.predictability==1));
+    %mean(Data.score(Data.difficulty==max(difficulty)) %in 4difflev 8 is a
+    %better difficulty than 10
+%     %lable subjects according to their performance in each difflevel--not so very useful atm
+%     for diffLev  = 1:length(difficulty)            
+%         prfGrpLl = mean(Data.score(Data.difficulty == diffLev));
+%         if prfGrpLl>=0.85    
+%             Data.perfGrp(Data.difficulty==difficulty(diffLev))=1; %perfect
+%         elseif prfGrpLl>=0.7
+%             Data.perfGrp(Data.difficulty==difficulty(diffLev))=2; %good
+%         elseif prfGrpLl>0.55
 %             Data.perfGrp(Data.difficulty==difficulty(diffLev))=3;
 %         else
-%             Data.perfGrp(Data.difficulty==difficulty(diffLev))=10; %bad
+%             Data.perfGrp(Data.difficulty==difficulty(diffLev))=4; %bad
 %         end
 %     end
+    
     AllData{sId} = Data;
-     %Omit subjects with score <50% in the min diff level
+    %Omit subjects with score <50% in the min diff level (if there is any)   
     ll = mean(Data.score(Data.difficulty == min(Data.difficulty)));
     if ll>0.5
         AllData{sId} = Data;
@@ -97,18 +105,17 @@ for sId=1:numel(SubjList)
 end
     
     
-save('AllData','AllData');
+save('AllData4Diff','AllData');
 
 
-% LengthCond=height(Data);
-% allCondMat=nan(numel(SubjList)*LengthCond,width(Data));
-% 
-% for sub=1:numel(SubjList)
-%      allCondMat(LengthCond*(sub-1)+1:sub*LengthCond,:)=table2array(AllData{sub});
-% end
-% PooledData=array2table(allCondMat,'VariableNames',ColNames([VarsOfInterest,19:28])); %#ok<NASGU> %for other than overtime you need to put conditionsMat to 20:end
-% save('PooledData','PooledData');
-% 
+LengthCond=height(Data);
+allCondMat=nan((numel(SubjList)*LengthCond),width(Data));
+
+for sub=1:numel(SubjList)
+     allCondMat(LengthCond*(sub-1)+1:sub*LengthCond,:)=table2array(AllData{sub});
+end
+PooledData=array2table(allCondMat,'VariableNames',ColNames([VarsOfInterest,18:end])); %18:end for 4difflev 19:end for quad
+save('PooledData4Diff','PooledData');
 
 %5=>subject's response,
 %6=>reaction time(RT),
